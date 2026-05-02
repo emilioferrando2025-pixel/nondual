0a8dc7f80f7bd3c6d5d5143e444b039f2a581613
emilioferrando2025-pixel <emilioferrando2025@gmail.com>
2026-05-02 11:31:18 +0200

    Primera subida

---
+++ nondual/academia/sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'nondual-academia-v1';
+const ASSETS = [
+  '/academia/',
+  '/academia/index.html',
+  '/academia/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400;1,600&family=DM+Sans:wght@300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/academia/')))
+  );
+});
